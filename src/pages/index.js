148290df98b0586da2f32c2707a35c44354e87c2
philipObinna <philip.obinnakwelu@decagon.dev>
2023-10-31 01:55:55 +0100

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import Login from "./Login";
+import Home from "./Home";
+import Profile from "./Profile";
+import Register from "./Register";
+import ResetPassword from "./ResetPassword";
+
+
+
+export {Login, Home, Profile, Register, ResetPassword}; 
+
---
+++ src/redux/postSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from "@reduxjs/toolkit";  
+import { dispatch } from "./Store";
+
+const initialState = {
+  posts: {},
+};
+
+const postSlice = createSlice({
+  name: 'posts',
+  initialState,
+  reducers: {
+    getPosts(state, action) {
+      state.posts = action.payload;
+    },
+  },
+});
+
+export default postSlice.reducer;
+
+export function SetPosts(post) {
+  return (dispatch, getState) => {
+    dispatch(postSlice.actions.getPosts(post));
+  };
+}
